625b955c531c81c4c44bea519275b5aa45de13bf
sm-mehedi <hasanmehedi63395@gmail.com>
2024-10-02 21:55:53 +0600

    Inserted api

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,12 @@
-console.log("meaow");
-console.log("meaow");
-console.log("meaow");
-console.log("meaow");
-console.log("meaow");
+const loadData = ()=>{
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(res => res.json())
+    .then(data => showData(data.categories))
+    .catch(err=>console.error('error',err));
+}
+
+const showData = (data)=>{
+console.log(data);
+
+}
+loadData();

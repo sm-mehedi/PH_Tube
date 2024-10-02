eaab8b695c155584c264db0d3cf7432ad06290f2
sm-mehedi <hasanmehedi63395@gmail.com>
2024-10-02 23:05:55 +0600

    video card part done

--- Scripts/script.js
+++ Scripts/script.js
@@ -18,4 +18,39 @@ const showData = (data)=>{
 
 
 }
+
+const loadVideos = ()=>{
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => showVideo(data.videos))
+    .catch(err=>console.error('error',err));
+}
+
+const showVideo = (data) =>{
+    const video = document.getElementById('videoSection');
+    data.forEach((element)=>{
+        const card = document.createElement("div");
+        card.classList.add("bg-white", "rounded-lg", "shadow-lg", "overflow-hidden", "transition-transform", "duration-300", "hover:scale-105", "m-4"); 
+        card.innerHTML=`
+        <figure>
+    <img
+      src="${element.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+        
+        `
+        video.append(card);
+
+    })
+   
+
+}
 loadData();
+loadVideos();
